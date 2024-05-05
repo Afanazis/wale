728b644f8a86dd64c5a492f2168339aa15349090
afanazis <afanazis87@gmail.com>
2024-05-05 17:16:18 +0700

    update login

--- data.js
+++ data.js
@@ -2,3 +2,21 @@ const namaSaya = 'mico andreas';
 let usia = 30.2
 console.log(namaSaya);
 console.log(usia)
+
+// alert ('silahkan login')
+
+// username&pass
+
+function func(){
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    if ( user == "micoandreas" && pass == "123456"){
+        window.location.assign('main-menu.html')
+        alert ('berhasil login !')
+        
+    }
+    else {
+    alert ('username atau password salah')
+    }
+}
+
